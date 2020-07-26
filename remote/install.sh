41cf080ed3a613a2bb95308f5472d07869adfdf3
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-07-27 00:23:20 +1000

    Add line 1 of NVM hack

--- remote/install.sh
+++ remote/install.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 # This hack makes the nvm binary available to this script.
-# export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
